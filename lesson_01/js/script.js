a91093cad824467a259c5b5829cc6cae3328d2ba
Valeriy <valeriy.rvv@gmail.com>
2018-03-30 13:48:26 +0700

    Редактирование файлов урока 1

--- lesson_01/js/script.js
+++ lesson_01/js/script.js
@@ -1,2 +1,22 @@
-let budget = 0;
-let shopTitle;
+var yourBudget = +prompt("Ваш бюджет?", "0");
+var yourShopName = prompt("Название вашего магазина?");
+
+mainList = {
+	 budget: yourBudget,
+	 shopName: yourShopName,
+	 shopGoods: ['Moloko', 'Hleb', 'Maslo'],
+	 employers: ['Ivan', 'Alexander', 'Dmitriy'],
+	 open: false
+};
+
+mainList.shopGoods[0] = prompt("Какой тип товаров будем продавать?");
+mainList.shopGoods[1] = prompt("Какой тип товаров будем продавать?");
+mainList.shopGoods[2] = prompt("Какой тип товаров будем продавать?");
+
+console.log("Название вашего магазина:", mainList.shopName);
+
+console.log("Массив shopGoods:", mainList.shopGoods);
+
+console.log("Ваш бюджет за месяц:", mainList.budget);
+
+console.log("Ваш бюджет за день:", mainList.budget/30);
