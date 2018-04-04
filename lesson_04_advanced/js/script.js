2bb34a9597aab8af0281d7b3edbbc7ea0dac186f
Valeriy <valeriy.rvv@gmail.com>
2018-04-05 01:01:13 +0700

    Редактирование файлов задания 4

--- lesson_04_advanced/js/script.js
+++ lesson_04_advanced/js/script.js
@@ -1,13 +1,41 @@
-let soldier = {
-	health: 400,
-	armor: 100
+let matrix = [[],[]];
+let matrixRange = [];
+let s = 0;
+
+//Генератор случайных целочисленных чисел в диапазоне от минимального до максимального
+function getRandomInRange(min,max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-let john = {
-	health: 100
+//Функция ввода размерности матрицы и заполнения ее элементов случайными числами
+function enterTheMatrix() {
+	let k = prompt("Введите размер матрицы из расчета AxB, где А - количество строк, B - количество столбцов");	
+	matrixRange = k.split("x"); 
+	for (i=0; i<matrixRange[0]; i++) {
+		var t = [];
+		for (j=0; j<matrixRange[1]; j++) {			
+			t[j] = getRandomInRange(1,5);				
+		}		
+		matrix[i] = t;
+	}
 }
 
-john.__proto__ = soldier;
+//Функиця нахождения суммых всех элементов матрицы
+function matrixSumm() {
+	for (i=0; i<matrixRange[0]; i++) {
+		var t = [];
+		t = matrix[i];		
+		for (j=0; j<matrixRange[1]; j++) {
+			s = s + t[j];
+		}
+	}	
+}
 
-console.log(john);
-console.log(john.armor);
+enterTheMatrix();
+
+console.log("Элементы матрицы:")
+console.log(matrix);
+
+matrixSumm();
+
+console.log("Сумма всех элементов матрицы равна: ", s);
