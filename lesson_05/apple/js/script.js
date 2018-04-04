7c416398abc979a64b4fb36dfee7b0b145e75e7e
Valeriy <valeriy.rvv@gmail.com>
2018-04-05 02:41:54 +0700

    Редактирование файлов задания 5

---
+++ lesson_05/apple/js/script.js
@@ -0,0 +1,8 @@
+let menuItem = document.getElementsByClassName('menu-item');
+let li = document.createElement('li');
+let div = document.createElement('div');
+
+console.log(menuItem[0]);
+
+//document.body.insertBefore(div, menuItem[1]);
+document.body.insertBefore(node: Node, child: Node)
