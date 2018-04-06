f23513c9d7205bc3f04b2d56662893c5c9d9bb43
Valeriy <valeriy.rvv@gmail.com>
2018-04-06 14:30:13 +0700

    Редактирование файлов задания 5

--- lesson_05/apple/js/script.js
+++ lesson_05/apple/js/script.js
@@ -1,8 +1,37 @@
-let menuItem = document.getElementsByClassName('menu-item');
-let li = document.createElement('li');
-let div = document.createElement('div');
+let menuItem = document.getElementsByClassName('menu-item'),
+				menu = document.getElementsByClassName('menu'),
+				advert = document.getElementsByClassName('adv'),
+				li = document.createElement('li'),
+				pageTitle = document.getElementById('title').textContent;
 
-console.log(menuItem[0]);
+//Замена пунктов меню
+menuItem[1].innerHTML = 'Второй пункт';
+menuItem[2].innerHTML = 'Третий пункт';
 
-//document.body.insertBefore(div, menuItem[1]);
-document.body.insertBefore(node: Node, child: Node)
+//Добавление пятого пункта меню
+li.innerHTML = 'Пятый пункт';
+li.classList.add('menu-item');
+menu[0].appendChild(li);
+
+//Замена фотового изображения
+document.body.style.backgroundImage = 'url(img/apple_true.jpg)';
+
+//Удаление блока с рекламой
+advert[0].style.visibility = 'hidden';
+
+//Добавление слова "подлинную" к залоговку
+let k = pageTitle.split(" ");
+let	t;
+for(i=0; i<k.length; i++) {
+	if (k[i] == 'только') {
+		t = i+1;	
+	}
+}
+k.splice(t,0,"подлинную");
+pageTitle = k.join(" ");
+document.getElementById('title').textContent = pageTitle;
+
+//Запись ответа на вопрос об отношении к технике Apple в поле "prompt"
+let regardApple = prompt("Напишите ваше отношение к технике Apple", '');
+document.getElementById('prompt').textContent = regardApple;
+
---
+++ lesson_05/js/script.js
@@ -0,0 +1,36 @@
+let openButton = document.getElementById('open-btn'),
+				nameValue = document.getElementsByClassName('name-value'),
+				budgetValue = document.getElementsByClassName('budget-value'),
+				goodsValue = document.getElementsByClassName('goods-value'),
+				itemsValue = document.getElementsByClassName('items-value'),
+				employersValue = document.getElementsByClassName('employers-value'),
+				discountValue = document.getElementsByClassName('discount-value'),
+				isopenValue = document.getElementsByClassName('isopen-value'),
+				goodsItems = document.getElementsByClassName('goods-item'),
+				goodsButtons = document.getElementsByTagName('button'),
+				chooseItem = document.querySelector('.main-functions .choose-item'),
+				timeValue = document.querySelector('.main-functions .time-value'),
+				countBudgetValue = document.querySelector('.main-functions .count-budget-value'),
+				hireEmployersItems = document.querySelectorAll('.main-functions .hire-employers-item');
+
+console.log(openButton);
+console.log(nameValue[0]);
+console.log(budgetValue[0]);
+console.log(goodsValue[0]);
+console.log(itemsValue[0]);
+console.log(employersValue[0]);
+console.log(discountValue[0]);
+console.log(isopenValue[0]);
+console.log(goodsItems[0]);
+console.log(goodsItems[1]);
+console.log(goodsItems[2]);
+console.log(goodsItems[3]);
+console.log(goodsButtons[1]);
+console.log(goodsButtons[2]);
+console.log(goodsButtons[3]);
+console.log(chooseItem);
+console.log(timeValue);
+console.log(countBudgetValue);
+console.log(hireEmployersItems[0]);
+console.log(hireEmployersItems[1]);
+console.log(hireEmployersItems[2]);
