d9b5195a4a6c6082a9ad694261d4bf9487329ef5
Valeriy <valeriy.rvv@gmail.com>
2018-04-11 18:39:50 +0700

    Редактирование файлов задания 9

--- lesson_09/js/script.js
+++ lesson_09/js/script.js
@@ -170,6 +170,31 @@ function animate(options) {
 let more = document.querySelector('.more');
 				overlay = document.querySelector('.overlay');
 				close = document.querySelector('.popup-close');
+				description_btn = document.getElementsByClassName('description-btn');
+
+description_btn[0].addEventListener('click', function() {
+				this.classList.add('more-splash');
+				overlay.style.display = "block";
+				document.body.style.overflow = 'hidden';
+});
+
+description_btn[1].addEventListener('click', function() {
+				this.classList.add('more-splash');
+				overlay.style.display = "block";
+				document.body.style.overflow = 'hidden';
+});
+
+description_btn[2].addEventListener('click', function() {
+				this.classList.add('more-splash');
+				overlay.style.display = "block";
+				document.body.style.overflow = 'hidden';
+});
+
+description_btn[3].addEventListener('click', function() {
+				this.classList.add('more-splash');
+				overlay.style.display = "block";
+				document.body.style.overflow = 'hidden';
+});
 
 more.addEventListener('click', function() {
 				this.classList.add('more-splash');
