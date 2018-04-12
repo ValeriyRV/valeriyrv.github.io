a692bbb515c71e53c0dc3702e72f99fdb7857683
Valeriy <valeriy.rvv@gmail.com>
2018-04-12 23:34:39 +0700

    Редактирование файлов задания 10

--- lesson_10/js/script.js
+++ lesson_10/js/script.js
@@ -1,19 +1,16 @@
 class options {
 			constructor(height, width, bg, fontSize, textAlign) {
-				this.height = '480px';
-				this.width = '960px';
-				this.bg = 'green';
-				this.fontSize = '20px';
-				this.textAlign = 'center';
+				this.height = document.getElementsByName('height')[0].value + 'px';
+				this.width = document.getElementsByName('width')[0].value + 'px';
+				this.bg = document.getElementsByName('bgcolor')[0].value;
+				this.fontSize = document.getElementsByName('fontsize')[0].value + 'px';
+				this.textAlign = document.getElementsByName('textalign')[0].value;
 			}
 
 			makeDiv() {
 				let div = document.createElement('div');
-
-				div.innerHTML = 'Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты.';
-
+				div.innerHTML = document.getElementsByName('div-text')[0].value;
 				div.style.cssText = `height:${this.height}; width:${this.width}; background-color:${this.bg}; font-size:${this.fontSize}; text-align:${this.textAlign};`;
-
 				document.body.appendChild(div);
 			}
 			
@@ -21,4 +18,13 @@ class options {
 
 		let element = new options;
 
-		element.makeDiv();
+element.makeDiv();
+
+let btn = document.getElementsByTagName('button')[0];
+
+btn.addEventListener('click', () => {
+					let a = document.body.children[2];
+					a.remove();
+					let b = new options;
+					b.makeDiv();					
+});
