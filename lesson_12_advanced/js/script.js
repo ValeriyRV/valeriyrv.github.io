1163021e7fec21859e7f2df9241c5df560214b0e
Valeriy <valeriy.rvv@gmail.com>
2018-04-16 05:12:07 +0700

    Редактирование файлов задания 12

--- lesson_12_advanced/js/script.js
+++ lesson_12_advanced/js/script.js
@@ -4,32 +4,51 @@ $(document).ready(function() {
 		$(".main_nav li:eq(1)").on('click', function() {
 					$(".overlay").fadeTo(1500, 1);
 				 $(".modal").animate({
+				 	opacity: 'show',
 				 	height: 'show'
-				 }, 3000);					
+				 }, 2500);					
 		});
 
 		//Получаем кнопку "Выбрать тур"
 		$(".main_btna").on('click', function() {
 					$(".overlay").fadeTo(1500, 1);				 					
-				 $(".modal").animate({	 	
-				 	height: 'show'
-				 }, 3000);				 
+				 $(".modal").animate({
+      opacity: 'show',
+      height: 'show'
+				 }, 2500);								 
 		});
 
 		//Получаем кнопку "Получить консультацию"
 		$(".contact").on('click', function() {
 					$(".overlay").fadeTo(1500, 1);
 				 $(".modal").animate({
+				 	opacity: 'show',
 				 	height: 'show'
-				 }, 3000);					
+				 }, 2500);					
 		});
 
 		//Обработчик кнопки закрытия модального окна
 		$(".close").on('click', function() {
 					$(".modal").animate({
+						opacity: 'hide',
 						height: 'hide'
-					}, 3000);
+					}, 2500);
 					$(".overlay").fadeTo(4500, 0);
 		});
+
+		//Асинхронная отправка сообщения из формы
+	$('.form-inline').submit(function(event) {
+				event.preventDefault();
+		  $.ajax({
+		  		type: "POST",
+		  		url: "mailer/smart.php",
+		 		 data: $(this).serialize()
+		  }).done(function() {
+				  $(this).find("input").val("");
+				  alert("Сообщение успешно отправлено");
+				  $(".form-inline").trigger("reset");
+		  });
+		  return false;
+  });
 
 });
