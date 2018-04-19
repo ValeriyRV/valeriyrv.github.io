3c1efdbaa2883d91bd86718eaf3df68fbc5fa11b
Valeriy <valeriy.rvv@gmail.com>
2018-04-19 19:49:16 +0700

    Редактирование файлов дипломного проекта

--- diploma/js/script.js
+++ diploma/js/script.js
@@ -1,6 +1,13 @@
 window.addEventListener('DOMContentLoaded', function() {
 
 	overlay = document.querySelector('.overlay');
+	main = document.querySelector('.main');
+
+	custom = document.querySelector('.custom');
+	custom_info = document.querySelector('.custom-info');
+	custom_char = document.querySelector('.custom-char');
+	custom_style = document.querySelector('.custom-style');
+
 	popup_btn = document.querySelector('.popup-btn');
 
 	reset = document.getElementById('reset');
@@ -10,6 +17,11 @@ window.addEventListener('DOMContentLoaded', function() {
 	// Дествия при нажатии кнопки "Создать" в модальном окне "Создать своего кандидата"
 	popup_btn.addEventListener('click', function() {
 		overlay.style.display = 'none';
+		main.style.display = 'none';
+		custom.style.display = 'flex';
+		custom_info.style.display = 'block';
+		custom_char.style.display = 'block';
+		custom_style.style.display = 'block';
 	});
 
 	// Дествия при нажатии кнопки "Сбросить результаты"
